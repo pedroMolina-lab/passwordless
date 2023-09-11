4ae386b8b349d9c7a8982221902b4af3109f179c
piter <pedri_molina@hotmail.com>
2023-09-11 12:00:11 -0300

    first-commit

---
+++ pages/api/index.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {} from "lib/firestore"
+
+export default function(req: NextApiRequest, res: NextApiResponse ){
+res.send("hola soy la api")
+}
